a24803f96c591d13f5fba6114db195f7c6c2476d
Android Lin <acen@dmp.com.tw>
2014-04-23 23:16:48 +0800

    Add LGPL license

--- v86dude.cpp
+++ v86dude.cpp
@@ -1,3 +1,25 @@
+/*
+  v86dude.cpp - 86Duino uploader
+  Copyright (c) 2013 Android Lin <acen@dmp.com.tw>. All right reserved.
+
+  This 86Duino uploader is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+  
+  (If you need a commercial license, please contact soc@dmp.com.tw 
+   to get more information.)
+*/
+
 #include "dmpcfg.h"
 #include <stdio.h>
 #include <stdlib.h>
