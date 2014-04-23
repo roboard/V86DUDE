fad82a98b13d86f177f359668f84bcc7d220b99b
Android Lin <acen@dmp.com.tw>
2014-04-23 23:46:07 +0800

    Modify to GPL license

--- v86dude.cpp
+++ v86dude.cpp
@@ -1,23 +1,23 @@
 /*
   v86dude.cpp - 86Duino uploader
-  Copyright (c) 2013 Android Lin <acen@dmp.com.tw>. All right reserved.
+  Part of the 86Duino project - http://www.86duino.com/
 
-  This 86Duino uploader is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+  Copyright (c) 2013
+  Android Lin <acen@dmp.com.tw>
 
-  This library is distributed in the hope that it will be useful,
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
 
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-  
-  (If you need a commercial license, please contact soc@dmp.com.tw 
-   to get more information.)
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
 #include "dmpcfg.h"
